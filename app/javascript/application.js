51a793dd39d232e2f239c6c4764f5bac7936d6e7
kenchaso <kenchasonakai@gmail.com>
2022-09-21 14:03:34 +0900

    fix: assets周りの読み込みの調整

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import * as bootstrap from "bootstrap"
+import * as bootstrap from "bootstrap/dist/js/bootstrap"
